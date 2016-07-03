64204c1e43af2398dde10b3a36c85710048b0f3f
vagrant <dg@darkog.com>
2016-07-03 20:52:23 +0000

    Add books page

--- resources/views/dashboard/books/index.blade.php
+++ resources/views/dashboard/books/index.blade.php
@@ -1,14 +1,5 @@
 @extends('layouts.dashboard')
 
 @section('content')
-    <div class="box">
-        <div class="box-header">
-            <h3 class="box-title">Books</h3>
-        </div>
-        <!-- /.box-header -->
-        <div class="box-body">
 
-        </div>
-        <!-- /.box-body -->
-    </div>
 @endsection
